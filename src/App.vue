a48274bc81144f5b4e6a96bfc2942d193fbe40b6
EricChong <zxh1245523241@gmail.com>
2025-09-19 16:48:08 +0800

    import and configure Vant-UI 2

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
+    <van-rate v-model="value"/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="less">
 
-nav {
-  padding: 30px;
+</style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      value: 3
     }
   }
 }
-</style>
+</script>
---
+++ src/utils/vant-ui.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+// Import UI From Vant
+import { Button, Rate } from 'vant'
+
+// Use Vant UI
+Vue.use(Button)
+Vue.use(Rate)
