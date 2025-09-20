9ada569cdec83803a48273fb404cf1e9223dd3ef
EricChong <zxh1245523241@gmail.com>
2025-09-20 13:56:40 +0800

    add common.less, fill login template

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 // Import UI From Vant
-import { Button, Rate, Tabbar, TabbarItem } from 'vant'
+import { Button, Rate, Tabbar, TabbarItem, NavBar } from 'vant'
 
 // Use Vant UI
 Vue.use(Button)
 Vue.use(Rate)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(NavBar)
